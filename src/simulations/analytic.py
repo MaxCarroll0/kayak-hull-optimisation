0139cbfe7cb924bbffa66b966ac3e8a4006c26e9
Max <mjvcarroll@gmail.com>
2025-12-11 15:29:46 +0000

    Analytic simulation runner
    - Just needs righting moment calculation

--- src/simulations/analytic.py
+++ src/simulations/analytic.py
@@ -6,8 +6,10 @@ import config
 from hull.hull import Hull
 import numpy as np
 import trimesh
-from trimesh import Trimesh
-from typing import Tuple, Any, Optional
+from trimesh import Scene
+from typing import Tuple, Any
+from simulations.params import Params
+from simulations.result import Result
 
 def _vec3d_to_tuple(vec: np.ndarray[Any, np.dtype[np.float64]]) -> Tuple[float, float, float]:
   return (vec[0], vec[1], vec[2])
@@ -33,16 +35,25 @@ def _iterate_draught(hull: Hull) -> Tuple[int, float]:
     loops += 1
     if loops > config.hyperparameters.buoyancy_max_iterations:
       raise RuntimeError("Analytic draught calculation failed to converge")
-    submerged = trimesh.intersections.slice_mesh_plane(hull.mesh, [0,0,-1], [0,0,draught])
+    submerged = trimesh.intersections.slice_mesh_plane(hull.mesh, [0,0,-1], [0,0,draught], cap=True)
     # TODO: Count air into displacement
     displacement = submerged.volume * config.constants.water_density
     diff = hull.mesh.mass - displacement
     draught += abs(diff) / hull.mesh.mass * (hull.mesh.bounds[2][1 if draught > 0 else 0] - draught)
   return loops, draught
 
-def _scene_draught(hull: Hull, draught: float):
-  T = np.linalg.inv(trimesh.geometry.plane_transform(origin=[0,0,1], normal=[0,0,1]))
-  trimesh.Scene([hull.mesh, trimesh.path.path.creation.grid(side=2, transform=T)])
+def _draught_proportion(hull: Hull, draught: float):
+  submerged = trimesh.intersections.slice_mesh_plane(hull.mesh, [0,0,-1], [0,0,draught], cap=True)
+  unsubmerged = trimesh.intersections.slice_mesh_plane(hull.mesh, [0,0,1], [0,0,draught], cap=True)
+  return unsubmerged.volume / (unsubmerged.volume + submerged.volume)
 
-def run(hull, fluid_dir, fluid_flow, angle):
-  return {"righting_moment":0.0, "drag":0.0, "cost":0.0}
+def _scene_draught(hull: Hull, draught: float) -> Scene:
+  T = np.linalg.inv(trimesh.geometry.plane_transform(origin=[0,0,1], normal=[0,0,1]))
+  return trimesh.Scene([hull.mesh, trimesh.path.path.creation.grid(side=2, transform=T)])
+
+def run(hull: Hull, params: Params) -> Result:
+  iterations, draught = _iterate_draught(hull)
+  return Result(righting_moment = 0, # TODO
+                draught_proportion = _draught_proportion(hull, draught),
+                scene = _scene_draught(hull, draught),
+                cost = hyperparameters.cost_analytic(iterations))
