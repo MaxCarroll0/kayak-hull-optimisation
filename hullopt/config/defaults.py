1c0ba7ba2daff976b214cd89879c7c0558ba7783
Ayain Fida <ayain.fida@outlook.com>
2026-01-10 16:37:25 +0500

    added hull opening bool;

--- hullopt/config/defaults.py
+++ hullopt/config/defaults.py
@@ -18,7 +18,7 @@ def _bathtub():
 # hull_bathtub = Hull.from_mesh(_bathtub())
     
 dummy_hull = Hull(Params(
-    density=900.0,
+    density=kayak_density,
     hull_thickness=0.005,
     length=2.6,
     beam=0.65,
@@ -33,3 +33,38 @@ dummy_hull = Hull(Params(
     cockpit_width=0.50,
     cockpit_position=0.50
 ))
+
+symmetric_default_hull = Hull(Params(
+    density=kayak_density,
+    hull_thickness=0.005,
+    length=2.6,
+    beam=0.65,
+    depth=0.35,
+    cross_section_exponent=2.0,
+    beam_position=0.50,
+    rocker_bow=0.25,
+    rocker_stern=0.25,
+    rocker_position=0.50,
+    rocker_exponent=2.0,
+    cockpit_length=0.85,
+    cockpit_width=0.60,
+    cockpit_position=0.50,
+    cockpit_opening=False
+))
+
+example_hull_1 = Params(
+    density=kayak_density,
+    hull_thickness=0.006,
+    length=3.0,
+    beam=0.70,
+    depth=0.38,
+    cross_section_exponent=1.5,
+    beam_position=0.55,
+    rocker_bow=0.25,
+    rocker_stern=0.20,
+    rocker_position=0.50,
+    rocker_exponent=2.0,
+    cockpit_length=0.80,
+    cockpit_width=0.55,
+    cockpit_position=0.50
+)
