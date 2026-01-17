a0d4fdc790bc7271cb96b08e680b430daae34043
Ayain Fida <ayain.fida@outlook.com>
2026-01-18 02:59:49 +0500

    example walkthrough updated

--- examples/example.py
+++ examples/example.py
@@ -3,15 +3,16 @@ import pickle
 from hullopt.simulations.analytic import run
 from hullopt.simulations.params import Params
 from hullopt.config.defaults import dummy_hull
+from hullopt.hull.utils import generate_random_hulls
 
 
+# First step: we generate random hulls with consraints
+hulls = generate_random_hulls(n=20, cockpit_opening=False, seed=42)
 
-
-# First step: We run a simulation for a given heel angle:
-for k in range(31):
-    result = run(dummy_hull, Params(heel=0.1*k))
-
-
+# Second step: We run a simulation for a given heel angle:
+for hull in hulls[:1]:
+    for k in range(31):
+        result = run(hull, Params(heel=0.1*k))
 
 with open("gp_data.pkl", "rb") as f_read:
     while True:
