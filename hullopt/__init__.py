2e6d83b3a5223a0b9e62c207cd02143c606a26c9
Max <mjvcarroll@gmail.com>
2026-01-19 10:41:48 +0000

    Oi, stop changing this Kilian

--- hullopt/__init__.py
+++ hullopt/__init__.py
@@ -1,2 +1,8 @@
+from .hull import Hull # Directly export Hull class (must be done before config)
+from hullopt import hull, config, simulations, gps, optimise, graphing
+
+# Aliases
+ParamsSim = simulations.Params
+ParamsHull = hull.Params
 
 __all__ = ["config", "hull", "gps", "simulations", "graphing", "optimise", "Hull", "ParamsSim", "ParamsHull"]
