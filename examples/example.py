b30aa11e5147b23fd2abab562a815a89a374c1aa
SchulzKilian <schulz.kilian@outlook.de>
2026-01-19 09:56:15 +0000

    Make the gp reloadable

--- examples/example.py
+++ examples/example.py
@@ -26,43 +26,48 @@ MODEL_PATH = "models/boat_gp.pkl"
 KERNEL_CONFIG = {"rocker_bow": "matern52", "heel": "periodic" }
 
 
+if not os.path.exists(MODEL_PATH):
+    # Initial data gathering for GP
+    if not os.path.exists(DATA_PATH):
+        print(os.getcwd())
+        from hullopt.hull.utils import generate_random_hulls
+        from hullopt.config.defaults import dummy_hull
+        
+        from hullopt.simulations.params import Params
+        from hullopt.simulations.analytic import run
+        hulls = generate_random_hulls(n=100, cockpit_opening=False, seed=42)
+        # Second step: We run a simulation for a given heel angle:
+        i = 0
+        for hull in hulls[:1]:
+            print("Simulating random hull: " + str(i))
+            i += 1
+            for k in range(301):
+                result = run(hull, Params(heel=0.1*k))
 
-# Initial data gathering for GP
-if not os.path.exists(DATA_PATH):
-    print(os.getcwd())
-    from hullopt.hull.utils import generate_random_hulls
-    from hullopt.config.defaults import dummy_hull
-    
-    from hullopt.simulations.params import Params
-    from hullopt.simulations.analytic import run
-    hulls = generate_random_hulls(n=100, cockpit_opening=False, seed=42)
-    # Second step: We run a simulation for a given heel angle:
-    i = 0
-    for hull in hulls[:1]:
-        print("Simulating random hull: " + str(i))
-        i += 1
-        for k in range(301):
-            result = run(hull, Params(heel=0.1*k))
+        
+    X_full, y_full, column_order = load_simulation_data(DATA_PATH)
 
-    
-X_full, y_full, column_order = load_simulation_data(DATA_PATH)
+    X_train, X_test, y_train, y_test = train_test_split(
+            X_full, y_full, test_size=0.2, random_state=42
 
-X_train, X_test, y_train, y_test = train_test_split(
-        X_full, y_full, test_size=0.2, random_state=42
+        )
+    gp = GaussianProcessSurrogate(ConfigurablePhysicsKernel(KERNEL_CONFIG), ZeroMeanPrior())
+    gp.save(MODEL_PATH)
 
+    rmse = compare_models({"cool":
+        gp},
+        X_train,
+        y_train,
+        X_test,
+        y_test,
+        column_order,
     )
-gp = GaussianProcessSurrogate(ConfigurablePhysicsKernel(KERNEL_CONFIG), ZeroMeanPrior())
+    print(f"Initial GP RMSE: {rmse}")
 
-rmse = compare_models({"cool":
-    gp},
-    X_train,
-    y_train,
-    X_test,
-    y_test,
-    column_order,
-)
-print(f"Initial GP RMSE: {rmse}")
-
+else:
+    with open(MODEL_PATH, 'rb') as f:
+        gp: GaussianProcessSurrogate = pickle.load(f)
+    _, _, column_order = load_simulation_data(DATA_PATH)
 
 
 @dataclass
